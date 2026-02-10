65a781874ea9ccb46bfcbd4891dc40bbb00baee1
abdullh wlayd <abdallhaamm2025@gmail.com>
2026-02-10 22:02:53 +0300

    Create main.dart

---
+++ homework2/main.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter layout demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter layout demo'),
+        ),
+        body: ListView(
+          children: const [
+            ImageSection(
+              image: 'images/lake.jpg',
+            ),
+            TitleSection(
+              name: 'Oeschinen Lake Campground',
+              location: 'Kandersteg, Switzerland',
+            ),
+            ButtonSection(),
+            TextSection(
+              description:
+                  'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. '
+                  'Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. '
+                  'A gondola ride from Kandersteg, followed by a half-hour walk through pastures '
+                  'and pine forest, leads you to the lake, which warms to 20 degrees Celsius in '
+                  'the summer. Activities enjoyed here include rowing, and riding the summer '
+                  'toboggan run.',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ImageSection extends StatelessWidget {
+  const ImageSection({super.key, required this.image});
+
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      image,
+      width: 600,
+      height: 240,
+      fit: BoxFit.cover,
+    );
+  }
+}
+
+class TitleSection extends StatelessWidget {
+  const TitleSection({
+    super.key,
+    required this.name,
+    required this.location,
+  });
+
+  final String name;
+  final String location;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    name,
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Text(
+                  location,
+                  style: TextStyle(color: Colors.grey[500]),
+                ),
+              ],
+            ),
+          ),
+          const Icon(Icons.star, color: Colors.red),
+          const Text('41'),
+        ],
+      ),
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final color = Theme.of(context).primaryColor;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _buildButton(color, Icons.call, 'CALL'),
+          _buildButton(color, Icons.near_me, 'ROUTE'),
+          _buildButton(color, Icons.share, 'SHARE'),
+        ],
+      ),
+    );
+  }
+
+  Column _buildButton(Color color, IconData icon, String text) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(icon, color: color),
+        Padding(
+          padding: const EdgeInsets.only(top: 8),
+          child: Text(
+            text,
+            style: TextStyle(fontSize: 12, color: color),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class TextSection extends StatelessWidget {
+  const TextSection({super.key, required this.description});
+
+  final String description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Text(description, softWrap: true),
+    );
+  }
+}
